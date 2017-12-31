acf8b04e0562e0a1deeb5d9e92c5bb3f7419c19b
Kohl R Dusenbery <31451970+kdusenbery@users.noreply.github.com>
2017-12-31 04:46:06 -0600

    Add files via upload

---
+++ Payrate Calc.py
@@ -0,0 +1,64 @@
+"""
+Kohl Dusenbery
+ENTD200
+Prof. Edeki
+Week 4
+July 29, 2017
+"""
+
+print("Problem 1A")
+print()
+
+hourlyRate = 8.52
+hoursWorked = 30
+ 
+pay = hoursWorked * hourlyRate
+ 
+print("Hourly pay rate:")
+print(hourlyRate)
+print()
+print("Number of hours worked:")
+print(hoursWorked)
+print()
+print("Pay amount:")
+print(pay)
+print()
+
+print("Problem 1B")
+print()
+ 
+hourlyRate = 11.54
+hoursWorked = 53
+ 
+pay = hoursWorked * hourlyRate
+ 
+print("Hourly pay rate:")
+print(hourlyRate)
+print()
+print("Number of hours worked:")
+print(hoursWorked)
+print()
+print("Pay amount:")
+print(pay)
+print()
+
+print("Problem 2")
+print()
+ 
+gasAmount = 12.17
+milesDriven = 298
+
+gasMileage = milesDriven / gasAmount
+ 
+print("Gallons of gas used:")
+print(gasAmount)
+print()
+print("Number of miles driven:")
+print(milesDriven)
+print()
+print("Average miles per gallon:")
+print("{0:0.2f}".format(gasMileage))
+ 
+ 
+ 
+
